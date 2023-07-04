59680d3e2928e2deca18ede1580cf09302d4562f
sfortier32 <sfortier@umass.edu>
2023-07-04 13:14:24 -0400

    beginnings of editor panel
    includes editor and info buttons

--- client/client.js
+++ client/client.js
@@ -28,11 +28,11 @@ plusDays.addEventListener("click", (e) => {
 
 const tableOptions = document.getElementById("table-options");
 const table = document.getElementById('table');
+const tableNames = ["Recipes", "Ingredients", "Tags"];
 
 
 async function displayHeaders(checked) {
     tableOptions.innerHTML = '';
-    const tableNames = ["Recipes", "Ingredients", "Tags"];
     for (let i = 0; i < tableNames.length; ++i) {
 
         const newLabel = document.createElement('label');
@@ -99,6 +99,56 @@ async function displayTable(tableName) {
 
         table.appendChild(newRow);
     }
+}
+
+
+// database editor
+const editorTables = document.getElementById("editor-tables");
+const editorButton = document.getElementById('editor-btn');
+const closeButton = document.getElementById('close-btn');
+const editor = document.getElementById("editor");
+
+
+editorButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    editor.style.display = "block";
+    displayEditorHeaders("Recipes");
+});
+
+closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    editor.style.display = "none";
+});
+
+function displayEditorHeaders(checked) {
+    editorTables.innerHTML = '';
+    for (let i = 0; i < tableNames.length; ++i) {
+
+        const newLabel = document.createElement('label');
+
+        const newInput = document.createElement('input');
+        newInput.id = tableNames[i].concat('-editor-table');
+        newInput.classList.add('radio-custom');
+        newInput.name = 'radio-editor-group';
+        newInput.type = 'radio';
+
+        if (tableNames[i] === checked) {
+            newInput.setAttribute("checked", "checked");
+        }
+        
+        newLabel.setAttribute("for", newInput.id);
+        newLabel.innerHTML = tableNames[i];
+
+        newInput.addEventListener("click", (e) => {
+            e.preventDefault();
+            displayEditorHeaders(tableNames[i]);
+        });
+
+        newLabel.appendChild(newInput);
+
+        editorTables.appendChild(newLabel);
+    }
+
 }
 
 
